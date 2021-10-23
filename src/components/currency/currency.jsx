8c64ea623a3ff698cc487b50997d6efb74d36af3
Nikolay Nikityuk <micolkaby@gmail.com>
2021-10-23 15:59:05 +0300

    refactor: rebuild currency selector

--- src/components/currency/currency.jsx
+++ src/components/currency/currency.jsx
@@ -1,4 +1,5 @@
 import './currency.css';
+import vector from '../../assets/images/vector.png';
 
 import React from 'react';
 import { connect } from 'react-redux';
@@ -7,9 +8,24 @@ import { graphql } from '@apollo/client/react/hoc';
 
 import { CURRENCIES_SYMBOLS } from '../../constants';
 import { getCurrency } from '../../redux/selectors';
-import { changeCurrency } from '../../redux/actions';
+import { changeCurrency, closeMiniCart } from '../../redux/actions';
 
 class Currency extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showOptionList: false,
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
 
   componentDidUpdate(prevProps) {
     const {currencies} = this.props.data;
@@ -20,31 +36,61 @@ class Currency extends React.Component {
     }
   }
 
-  handleChangeCurrency(e) {
-    this.props.changeCurrency(e.target.value);
-  }
+  handleClickOutside = e => {
+    if (
+      !e.target.classList.contains("custom-select-option") &&
+      !e.target.classList.contains("selected-text")
+    ) {
+      this.setState({ showOptionList: false });
+    }
+  };
+
+  handleListDisplay = () => {
+    this.setState(prevState => {
+      return { showOptionList: !prevState.showOptionList };
+    });
+  };
+
+  handleOptionClick = e => {
+    this.props.changeCurrency(e.target.getAttribute("data-name"));
+    this.setState({ showOptionList: false });
+  };
 
   render () {
-    const {error, loading, currencies} = this.props.data;
+    const { error, loading, currencies } = this.props.data;
+    const { showOptionList } = this.state;
 
     if (error) return <div>Error: {error}</div>
     if (loading) return <div>isLoading...</div>
 
     return (
-      <div>
-        <select 
-          value={this.props.currency} 
-          onChange={this.handleChangeCurrency.bind(this)} 
-          className="currency_select"
+      <div className="custom-select-container" onClick={() => {this.props.closeMiniCart()}}>
+        <div
+          className="selected-text"
+          onClick={this.handleListDisplay}
         >
-          {currencies.map(el => {
-            return <option key={el} value={el}>{`${CURRENCIES_SYMBOLS[el]} ${el}`}</option>
-          })}
-        </select>
+        {CURRENCIES_SYMBOLS[this.props.currency]}
+        <img src={vector} alt="vector" className={showOptionList ? "vector-icon_transform" : "vector-icon" } />
       </div>
+      {showOptionList && (
+        <ul className="select-options">
+          {currencies.map(option => {
+            return (
+              <li
+                className="custom-select-option"
+                data-name={option}
+                key={option}
+                onClick={this.handleOptionClick}
+              >
+                {`${CURRENCIES_SYMBOLS[option]} ${option}`}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
     );
   }
-  
 }
 
 const withCurrenciesQuery = graphql(gql`
@@ -59,4 +105,4 @@ const mapStateToProps = state => {
   return getCurrency(state)
 };
 
-export default connect(mapStateToProps, { changeCurrency })(CurrencyWithData);
+export default connect(mapStateToProps, { changeCurrency, closeMiniCart })(CurrencyWithData);
